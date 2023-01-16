5d6da4e48ce5afd29cf4f79ac639c478b41e5839
arikawaaaaaaaaa <taisei-arikawa-k21h@stu.kbc.ac.jp>
2023-01-16 11:07:42 +0900

    GameBase

---
+++ tukemonogame/tukemonogame/Title.cpp
@@ -0,0 +1,75 @@
+#include "Title.h"
+#include "PadInput.h"
+
+Title::Title() {
+	JoyPadX = -1;
+	JoyPadY = -1;
+	OldY = -1;
+	flgY = 0;
+
+	Credit = false;
+
+	//メニュー選択
+	MenuY = 0;
+	MenuNum = 0;
+
+}
+
+AbstractScene* Title::Update() {
+	InitPad();
+
+	if (!Credit) {
+		//メニューカーソル（三角形）の座標
+		MenuY = MenuNum * 90;
+		//メニューカーソル移動処理
+		//下方向
+		if (flgY == 0 && JoyPadY < -MARGIN) {
+			if (++MenuNum > 2) MenuNum = 0;
+		}
+		//上方向
+		if (flgY == 0 && JoyPadY > MARGIN) {
+			if (--MenuNum < 0) MenuNum = 2;
+		}
+
+		//メニュー選択
+		if (PAD_INPUT::OnClick(XINPUT_BUTTON_B)) {
+			switch (MenuNum)
+			{
+			case 0:
+				//return new GameSelect();	//ゲーム選択画面へ
+				break;
+			case 1:
+				Credit = true;	//ゲーム選択画面へ
+				break;
+			case 2:
+				return nullptr;             //ゲーム終了
+				break;
+			}
+		}
+	}
+	else {
+		if (PAD_INPUT::OnClick(XINPUT_BUTTON_B)) {
+			Credit = false;
+		}
+	}
+
+	return this;
+}
+
+void Title::Draw() const {
+	DrawString(500, 255, "unti no zigoku", 0xffffff);
+	DrawFormatString(500, 270, 0xffffff, "%d", MenuNum);
+}
+
+void Title::InitPad() {
+
+	JoyPadY = PAD_INPUT::GetPadThumbLY();
+
+	if (OldY > MARGIN && JoyPadY > MARGIN) {
+		flgY = 1;
+	}
+	else if (OldY < -MARGIN && JoyPadY < -MARGIN)flgY = 1;
+	else flgY = 0;
+
+	OldY = JoyPadY;
+}
